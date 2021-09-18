df7fde6ae962058d130d42bd7d0c437476231f84
nico3587 <nicolas.hallynck@gmail.com>
2021-09-18 14:58:15 +0200

    Add project files.

---
+++ Panorama_QRCode_Generator/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace Panorama_QRCode_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.btnSearchApp = new System.Windows.Forms.Button();
+            this.textBoxAppPath = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBoxStatus = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(580, 162);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(226, 55);
+            this.btnGenerate.TabIndex = 21;
+            this.btnGenerate.Text = "Générer QR Code";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(25, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(406, 33);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Mot de passe de l\'application :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(25, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(357, 33);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Chemin vers l\'application :";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPassword.Location = new System.Drawing.Point(24, 173);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(420, 44);
+            this.textBoxPassword.TabIndex = 18;
+            // 
+            // btnSearchApp
+            // 
+            this.btnSearchApp.Location = new System.Drawing.Point(977, 71);
+            this.btnSearchApp.Name = "btnSearchApp";
+            this.btnSearchApp.Size = new System.Drawing.Size(82, 44);
+            this.btnSearchApp.TabIndex = 17;
+            this.btnSearchApp.Text = "...";
+            this.btnSearchApp.UseVisualStyleBackColor = true;
+            this.btnSearchApp.Click += new System.EventHandler(this.btnSearchApp_Click);
+            // 
+            // textBoxAppPath
+            // 
+            this.textBoxAppPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAppPath.Location = new System.Drawing.Point(26, 71);
+            this.textBoxAppPath.Name = "textBoxAppPath";
+            this.textBoxAppPath.Size = new System.Drawing.Size(922, 44);
+            this.textBoxAppPath.TabIndex = 16;
+            this.textBoxAppPath.Text = "C:\\Users\\Public\\Documents\\Codra\\Panorama E2\\ST-Demo";
+            // 
+            // textBoxStatus
+            // 
+            this.textBoxStatus.Location = new System.Drawing.Point(24, 246);
+            this.textBoxStatus.Multiline = true;
+            this.textBoxStatus.Name = "textBoxStatus";
+            this.textBoxStatus.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxStatus.Size = new System.Drawing.Size(1035, 412);
+            this.textBoxStatus.TabIndex = 23;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "\"PDF files\"|*.pdf";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1112, 706);
+            this.Controls.Add(this.textBoxStatus);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.btnSearchApp);
+            this.Controls.Add(this.textBoxAppPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button btnSearchApp;
+        private System.Windows.Forms.TextBox textBoxAppPath;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBoxStatus;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Panorama_QRCode_Generator/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using CODRA.Panorama.Persist;
+using System.IO;
+using QRCoder;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace Panorama_QRCode_Generator
+{
+    public partial class Form1 : Form
+    {
+        static IPersistCtrl MyPersist;
+        static IPrsApplicationManager MyAppManager;
+
+        List<string> qrCodeList = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            MyPersist = new PersistCtrl();
+            MyPersist.DesignMode = false;
+            MyAppManager = (IPrsApplicationManager)MyPersist;
+        }
+        private void AddLineStatus(string text)
+        {
+            textBoxStatus.Text = text + "\x0d\x0a" + textBoxStatus.Text;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            ProtectionType res = 0;
+            try
+            {
+                textBoxStatus.Text = "";
+                AddLineStatus("Ouverture de l'application...");
+                IPrsApplication MyApp = MyAppManager.Open(@textBoxAppPath.Text, 0);
+
+                if ((MyApp.AccessMode == ProtectionType.WRITE_PROTECTED) || (MyApp.AccessMode == ProtectionType.READWRITE_PROTECTED))
+                {
+                    res = MyApp.UnlockApplication("", textBoxPassword.Text);
+
+                    if (res != ProtectionType.NOT_PROTECTED)
+                    {
+                        // Wrong password or read protection ! 
+                        AddLineStatus("Erreur de mot de passe !!");
+                        return;
+                    }
+                }
+
+                if (MyApp.Status == ApplicationStatus.APP_OK)
+                {
+                    AddLineStatus("Application ouverte");
+                    textBoxAppPath.Enabled = false;
+                    btnSearchApp.Enabled = false;
+                    btnGenerate.Enabled = false;
+                }
+
+                AddLineStatus("Recherche des QR Code...");
+
+                //Scan unit in search of application QRCode
+                if (MyApp.UnitsEx.Count > 0)
+                {
+                    foreach (IPrsUnit MyUnit in MyApp.UnitsEx)
+                    {
+                        AddLineStatus("Scan unit " + MyUnit.Name);
+                        if (MyUnit.Name != "$_LIBRARY" && MyUnit.Name != "$_SYSTEM" && MyUnit.Name != "$_PARAMETERS" && MyUnit.Name != "$_WORKSTATION")
+                            ScanUnit(MyUnit);
+                    }
+                }
+
+                //Create PDF file with QRCode
+                CreatePdfFile(qrCodeList);
+
+                AddLineStatus("Fermeture de l'application...");
+                MyAppManager.Close(textBoxAppPath.Text);
+                MyApp.Unload();
+                MyPersist.Unload();
+                AddLineStatus("Application fermée");
+
+                textBoxAppPath.Enabled = true;
+                btnSearchApp.Enabled = true;
+                btnGenerate.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                string message;
+
+                int iErrorCode = unchecked((int)0x80040005); // initialize with E_FAIL
+
+                ExternalException ExtEx = (ExternalException)ex;
+
+                if (null != ExtEx)
+                {
+                    iErrorCode = ExtEx.ErrorCode; // get PERSIST Exception if available
+                }
+                else if (ex is UnauthorizedAccessException)
+                {
+                    iErrorCode = unchecked((int)0x80070005); // E_ACCESSDENIED
+                }
+                else if (ex is ArgumentException)
+                {
+                    iErrorCode = unchecked((int)0x80070057); // E_INVALIDARG
+                }
+                message = MyPersist.GetErrorText(iErrorCode, 0);
+
+                MessageBox.Show(message, "Erreur ouverture application", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                MyPersist.Unload();
+            }
+        }
+
+        private void ScanUnit(IPrsUnit unit)
+        {
+            if (unit.UnitsEx.Count > 0)
+            {
+                foreach (IPrsUnit MyUnit in unit.UnitsEx)
+                {
+                    AddLineStatus("Scan unit " + MyUnit.Name);
+                    ScanUnit(MyUnit);
+
+                    if (MyUnit.FullName.StartsWith("/$_LIBRARY") == false)
+                        MyUnit.Unload();
+                }
+            }
+            if (unit.ObjectsEx.Count > 0)
+            {
+                foreach (IPrsObject MyObj in unit.ObjectsEx)
+                {
+                    AddLineStatus("Scan obj " + MyObj.Name);
+                    ScanObject(MyObj);
+                }
+            }
+
+        }
+
+        private void ScanObject(IPrsObject obj)
+        {
+            try
+            {
+                if (obj.get_CollectionEx("Objects").Count > 0)
+                {
+                    foreach (IPrsObject subObj in obj.get_CollectionEx("Objects"))
+                    {
+                        AddLineStatus("Scan sub obj " + subObj.Name);
+                        ScanObject(subObj);
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+            if (obj.ClassName == "SynoptHMI_Mobile" && obj.ModuleName == "CODRA_Mobile")
+            {
+                object[] values = null;
+                string[] names = null;
+                bool[] defaults = null;
+                string[] links = null;
+                ValueSourceType[] VSTs = null;
+
+                obj.get_ValuesEx(ref values, ref names, ref defaults, ref links, ref VSTs, GetValuesExOptions.GVO_NAMES | GetValuesExOptions.GVO_VALUES);
+
+                for (int i = 0; i < names.Length; i++)
+                {
+                    if (names[i] == "QRCode")
+                    {
+                        if (values[i].ToString() != "")
+                            qrCodeList.Add(values[i].ToString());
+                    }
+                }
+            }
+            
+        }
+
+        private void btnSearchApp_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBoxAppPath.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void CreatePdfFile(List<string> qrCodes)
+        {
+            int qrIndex = 0, qrLine = 0;
+            //Create a new PDF document
+            PdfDocument document = new PdfDocument();
+            document.Info.Title = "My PDF file";
+
+            //Create empty page
+            PdfPage page = document.AddPage();
+
+            //Get an XGraphics object for drawing
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+            //Create a font
+            XFont font = new XFont("Verdana", 10, XFontStyle.Bold);
+
+            foreach (string qr in qrCodes)
+            {
+                int xPoint, yPoint;
+
+                //Set y position for QRCode
+                if (qrLine % 2 == 0)
+                    yPoint = 440;
+                else
+                    yPoint = 50;
+
+                //Add page if needed
+                if (qrLine % 2 == 0 && qrIndex % 2 == 0 && qrLine != 0)
+                {
+                    page = document.AddPage();
+                    gfx = XGraphics.FromPdfPage(page);
+                }
+                    
+                //Set x position for QRCode
+                if (qrIndex % 2 == 0)
+                {
+                    xPoint = 0;
+                }  
+                else
+                {
+                    xPoint = 290;
+                    qrLine++;
+                }
+
+                qrIndex++;
+
+                //Generate QRCode
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(qr, QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new QRCode(qrCodeData);
+                Bitmap qrCodeImage = qrCode.GetGraphic(10);
+
+                //Save QRCode in image file
+                qrCodeImage.Save("qrCode" + qrIndex + ".bmp");
+
+                //Add image file to PDF file
+                using (XImage image = XImage.FromFile("qrCode" + qrIndex + ".bmp"))
+                {
+                    gfx.DrawImage(image, new XPoint(xPoint, yPoint));
+                }
+                //Delete image file
+                File.Delete("qrCode" + qrIndex + ".bmp");
+
+                //Draw QRCode text
+                gfx.DrawString(qr, font, XBrushes.Black, new XRect(xPoint + 100, yPoint + 300, 100, 20), XStringFormats.Center);
+
+            }
+
+            //Open select file dialog
+            saveFileDialog1.ShowDialog();
+            //Save PDF file
+            string filename = saveFileDialog1.FileName;
+            document.Save(filename);
+            Process.Start(filename);
+        }
+    }
+}
